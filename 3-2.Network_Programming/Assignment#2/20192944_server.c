708d9e092c1b458ee53150e9c2db8bf6d7d55a6e
oxdjww <oxdjww@gmail.com>
2024-04-13 15:42:53 +0900

    Remove func wait() to run multi-client

--- 3-2.Network_Programming/Assignment#2/20192944_server.c
+++ 3-2.Network_Programming/Assignment#2/20192944_server.c
@@ -106,7 +106,7 @@ int main(int argc, char **argv)
             errProc("fork");
         else
         { // Parent process
-            wait(NULL);
+            // wait(NULL);
             close(clntSd);
         }
     }
