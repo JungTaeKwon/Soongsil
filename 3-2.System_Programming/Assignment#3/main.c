047b57b25b4a4fa8016cb347bb2537c1e57e1ed3
oxdjww <oxdjww@gmail.com>
2024-05-11 00:20:54 +0900

    Add System Programming Assignment#3 fread, fwrite

--- 3-2.System_Programming/Assignment#3/main.c
+++ 3-2.System_Programming/Assignment#3/main.c
@@ -1,19 +1,108 @@
 #include "mystdio.h"
 #include <unistd.h>
 
+void fopen_test();
+void fread_test();
+void fwrite_test();
+
 int main()
 {
-    // printf("[*] 2024 System Programming, Assignment#3\n");
+    fopen_test();
 
+    fread_test();
+
+    fwrite_test();
+
+    return 0;
+}
+
+void fopen_test()
+{
     // my_fopen test
-    MYFILE *fp = my_fopen("sample.txt", "r");
+    write(stdout, "[*] MY_FOPEN_TEST\n", strlen("[*] MY_FOPEN_TEST\n"));
+    MYFILE *fp = my_fopen("fopen_test.txt", "r+");
     if (fp == NULL)
     {
-        write(STDERR_FILENO, "fail\n", 5);
+        write(stderr, "my_fopen fail\n", 14);
     }
     else
     {
-        write(STDOUT_FILENO, "success\n", 8);
+        write(stdout, "my_fopen success\n", strlen("my_fopen success\n"));
     }
-    return 0;
+
+    write(stdout, "\n\n", 2);
+    return;
+}
+
+void fread_test()
+{
+    write(stdout, "[*] MY_FREAD_TEST\n", strlen("[*] MY_FREAD_TEST\n"));
+    MYFILE *fp = my_fopen("fread_test.txt", "r+");
+    // my_fread test
+    char readBuffer[1024];
+
+    size_t readCount = my_fread(readBuffer, sizeof(char), sizeof(readBuffer) - 1, fp);
+    if (readCount == 0)
+    {
+        write(stdout, "my_fread fail\n", 14);
+    }
+    else
+    {
+        readBuffer[readCount] = '\0';
+        write(stdout, "my_fread success\n", 17);
+        write(stdout, readBuffer, readCount);
+    }
+
+    write(stdout, "\n\n", 2);
+    return;
+}
+
+void fwrite_test()
+{
+    // my_fwrite test
+    write(stdout, "[*] MY_FWRITE_TEST\n", strlen("[*] MY_FWRITE_TEST\n"));
+    MYFILE *fp = my_fopen("fwrite_test.txt", "r+");
+    char readBuffer[1024];
+    char writeBuffer[1024];
+    int readCount = 0;
+
+    write(stdout, "[*] Before my_fwrite()\n", strlen("[*] Before my_fwrite()\n"));
+    readCount = my_fread(readBuffer, sizeof(char), sizeof(readBuffer) - 1, fp);
+    if (readCount == 0)
+    {
+        write(stderr, "my_fread fail\n", strlen("my_fread fail\n"));
+    }
+    else
+    {
+        readBuffer[readCount] = '\0';
+        write(stdout, "my_fread success\n", strlen("my_fread success\n"));
+        write(stdout, readBuffer, readCount);
+    }
+
+    size_t writeCount = my_fwrite(writeBuffer, sizeof(char), strlen(writeBuffer), fp);
+    if (writeCount == 0)
+    {
+        write(stderr, "my_fwrite fail\n", strlen("my_fwrite fail\n"));
+    }
+    else
+    {
+        write(stdout, "my_fwrite success\n", strlen("my_fwrite success\n"));
+    }
+
+    write(stdout, "[*] After my_fwrite()\n", strlen("[*] After my_fwrite()\n"));
+    fp = my_fopen("fwrite_test.txt", "r+");
+    readCount = my_fread(readBuffer, sizeof(char), sizeof(readBuffer) - 1, fp);
+    if (readCount == 0)
+    {
+        write(stderr, "my_fread fail\n", strlen("my_fread fail\n"));
+    }
+    else
+    {
+        readBuffer[readCount] = '\0';
+        write(stdout, "my_fread success\n", strlen("my_fread success\n"));
+        write(stdout, readBuffer, readCount);
+    }
+
+    write(stdout, "\n\n", 2);
+    return;
 }
--- 3-2.System_Programming/Assignment#3/mystdio.h
+++ 3-2.System_Programming/Assignment#3/mystdio.h
@@ -1,6 +1,9 @@
+#include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFSIZE 1024
 #define EOF -1
@@ -36,19 +39,19 @@ MYFILE *my_fopen(const char *pathname, const char *mode)
     }
     else if (strcmp(mode, "w") == 0)
     {
-        flags = O_WRONLY | (O_CREAT | O_TRUNC);
+        flags = O_WRONLY | O_CREAT | O_TRUNC;
     }
     else if (strcmp(mode, "w+") == 0)
     {
-        flags = O_RDWR | (O_CREAT | O_TRUNC);
+        flags = O_RDWR | O_CREAT | O_TRUNC;
     }
     else if (strcmp(mode, "a") == 0)
     {
-        flags = (O_WRONLY | O_APPEND) | (O_CREAT | O_TRUNC);
+        flags = O_WRONLY | O_APPEND | O_CREAT | O_TRUNC;
     }
     else if (strcmp(mode, "a+") == 0)
     {
-        flags = O_RDWR | (O_WRONLY | O_APPEND) | (O_CREAT | O_TRUNC);
+        flags = O_RDWR | O_WRONLY | O_APPEND | O_CREAT | O_TRUNC;
     }
     else
     {
@@ -64,4 +67,65 @@ MYFILE *my_fopen(const char *pathname, const char *mode)
     MYFILE *fp = (MYFILE *)malloc(sizeof(MYFILE));
     fp->fd = fd;
     return fp;
+}
+
+size_t my_fread(void *ptr, size_t size, size_t nmemb, MYFILE *stream)
+{
+    if (stream == NULL || ptr == NULL)
+    {
+        return 0;
+    }
+
+    size_t totalBytesToRead = size * nmemb;
+    size_t bytesRead = 0;
+    char *buffPtr = (char *)ptr;
+
+    while (bytesRead < totalBytesToRead)
+    {
+        ssize_t result = read(stream->fd, buffPtr + bytesRead, totalBytesToRead - bytesRead);
+        if (result < 0)
+        {
+            stream->eof = 1;
+            break;
+        }
+        else if (result == 0)
+        {
+            stream->eof = 1;
+            break;
+        }
+        else
+        {
+            bytesRead += result;
+            write(STDOUT_FILENO, buffPtr + bytesRead - result, result);
+        }
+    }
+
+    return bytesRead / size;
+}
+
+size_t my_fwrite(const void *ptr, size_t size, size_t nmemb, MYFILE *stream)
+{
+    if (stream == NULL || ptr == NULL)
+    {
+        return 0;
+    }
+
+    size_t totalBytesToWrite = size * nmemb;
+    size_t bytesWritten = 0;
+    const char *buffPtr = (const char *)ptr;
+
+    while (bytesWritten < totalBytesToWrite)
+    {
+        ssize_t result = write(stream->fd, buffPtr + bytesWritten, totalBytesToWrite - bytesWritten);
+        if (result < 0)
+        {
+            break;
+        }
+        else
+        {
+            bytesWritten += result;
+        }
+    }
+
+    return bytesWritten / size;
 }
