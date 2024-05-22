1e0677aa7df04fe41c1f5a8ad737d6c9ff37aaa1
oxdjww <oxdjww@gmail.com>
2024-05-22 18:30:49 +0900

    pipe communication

--- 3-2.System_Programming/Assignment#3/Bonus_Assignment/20192944.c
+++ 3-2.System_Programming/Assignment#3/Bonus_Assignment/20192944.c
@@ -1,134 +1,96 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define BUFSIZE 1024
-#define EOF -1
-#define SEEK_SET 0
-#define SEEK_CUR 1
-#define SEEK_END 2
-
-#define PROCESS_COUNT 2
-
-int checkOffset(char *str);
+#define BUFFER_SIZE 256
+#define PROCESS_COUNT 4
 
 int main(int argc, char *argv[])
 {
-    pid_t pids[PROCESS_COUNT];
-
+    pid_t pids[5];
     int pipefd[5][2];
+    pids[0] = getpid();
+    int pipeNum;
     for (int i = 0; i < 5; i++)
     {
         if (pipe(pipefd[i]) == -1)
         {
-            perror("[*] pipe");
-            exit(EXIT_FAILURE);
+            perror("[*] pipe creation error");
+            exit(1);
         }
     }
 
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < PROCESS_COUNT; i++)
     {
-        pids[i] = fork();
-        if (pids[i] < 0)
+        if (pids[0] == getpid())
         {
-            perror("[*] Fork failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pids[i] == 0)
-        {
-            // Child process code
-            FILE *fp = fopen(argv[1], "r");
-            if (fp == NULL)
+            // printf("Fork in %d process\n", getpid());
+            pids[i + 1] = fork();
+            if (pids[i + 1] == 0)
             {
-                perror("[*] File opening failed");
-                exit(EXIT_FAILURE);
+                pids[i + 1] = getpid();
             }
-
-            char offset[1024];
-            read(pipefd[i][0], offset, sizeof(offset));
-            int iter = checkOffset(offset);
-
-            char *location;
-            char buffer[1024];
-            while (iter--)
-                location = fgets(buffer, BUFSIZE, fp);
-
-            fgets(buffer, BUFSIZE, fp);
-            pids[i] = getpid();
-            printf("%d %s\n", pids[i], buffer);
-
-            if (feof(fp))
-            {
-                printf("%d Read all data\n", pids[i]);
-                exit(0);
-            }
-
-            close(pipefd[i][1]);
-            fclose(fp);
-        }
-        else
-        {
-            // Parent process code
-            FILE *fp = fopen(argv[1], "r");
-            if (fp == NULL)
-            {
-                perror("[*] File opening failed");
-                exit(EXIT_FAILURE);
-            }
-
-            close(pipefd[i][0]); // 읽기 끝 닫기
-            char buffer[BUFSIZE];
-            char *offset = fgets(buffer, BUFSIZE, fp);
-            if (offset != NULL)
-            {
-                printf("%d %s\n", getpid(), buffer);
-            }
-
-            write(pipefd[i][1], offset, sizeof(offset));
-            fclose(fp);
-            close(pipefd[i][1]); // 쓰기 끝 닫기
         }
     }
 
-    for (int i = 0; i < PROCESS_COUNT - 1; i++)
+    for (int i = 0; i < PROCESS_COUNT + 1; i++)
+    {
+        if (pids[i] == getpid())
+        {
+            pipeNum = i;
+        }
+    }
+
+    char tmpBuff[BUFFER_SIZE];
+
+    if (getpid() == pids[0])
+    {
+        FILE *fp = fopen(argv[1], "r");
+        if (fp == NULL)
+        {
+            perror("[*] fopen error");
+            exit(1);
+        }
+
+        fgets(tmpBuff, BUFFER_SIZE, fp);
+        printf("%d %s\n", getpid(), tmpBuff);
+
+        // printf("Next pipe: %d\n", pipeNum + 1);
+        write(pipefd[pipeNum + 1][1], "start", 5);
+        fclose(fp);
+    }
+    int cnt = 5;
+    while (cnt--)
+    {
+        // printf("pid: %d, pipeNum: %d\n", getpid(), pipeNum);
+
+        FILE *fp = fopen(argv[1], "r");
+        if (fp == NULL)
+        {
+            perror("[*] fopen error");
+            exit(1);
+        }
+        // printf("waiting from %d\n", pipeNum);
+        read(pipefd[pipeNum][0], tmpBuff, sizeof(tmpBuff));
+        // printf("%d received %s\n", getpid(), tmpBuff);
+
+        fgets(tmpBuff, BUFFER_SIZE, fp);
+        printf("%d %s\n", getpid(), tmpBuff);
+
+        // printf("Next pipe: %d\n", pipeNum + 1);
+        write(pipefd[pipeNum + 1][1], "next", 4);
+        fclose(fp);
+    }
+
+    for (int i = PROCESS_COUNT; i >= 0; i--)
     {
         int status;
         pid_t pid = waitpid(pids[i], &status, 0);
         printf("%d I'm exiting...\n", pid);
     }
-    printf("%d I'm exiting...\n", getpid());
+    printf("%d I'm exiting..\n", getpid());
     return 0;
-}
-
-int checkOffset(char *str)
-{
-    str[strlen(str) - 1] = '\0';
-    if (strcmp(str, "first") == 0)
-        return 1;
-    else if (strcmp(str, "second") == 0)
-        return 2;
-    else if (strcmp(str, "third") == 0)
-        return 3;
-    else if (strcmp(str, "fourth") == 0)
-        return 4;
-    else if (strcmp(str, "fifth") == 0)
-        return 5;
-    else if (strcmp(str, "sixth") == 0)
-        return 6;
-    else if (strcmp(str, "seventh") == 0)
-        return 7;
-    else if (strcmp(str, "eighth") == 0)
-        return 8;
-    else if (strcmp(str, "nineth") == 0)
-        return 9;
-    else if (strcmp(str, "tenth") == 0)
-        return 10;
-    else
-        return -1;
 }
