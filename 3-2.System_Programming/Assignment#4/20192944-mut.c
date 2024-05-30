476447924c321284fbda035499873ff6084e3fc0
oxdjww <oxdjww@gmail.com>
2024-05-30 11:23:56 +0000

    Add mutex

---
+++ 3-2.System_Programming/Assignment#4/20192944-mut.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+
+#define THREADS 4
+#define N 3000
+
+int primes[N];
+int pflag[N];
+int total = 0;
+pthread_mutex_t mutex;
+
+int is_prime(int v)
+{
+    int i;
+    int bound = floor(sqrt((double)v)) + 1;
+    for (i = 2; i < bound; i++)
+    {
+        /* No need to check against known composites */
+        if (!pflag[i])
+            continue;
+        if (v % i == 0)
+        {
+            pthread_mutex_lock(&mutex);
+            pflag[v] = 0;
+            pthread_mutex_unlock(&mutex);
+            return 0;
+        }
+    }
+    return (v > 1);
+}
+
+void *work(void *arg)
+{
+    int start;
+    int end;
+    int i;
+    start = (N / THREADS) * (*(int *)arg);
+    end = start + N / THREADS;
+    for (i = start; i < end; i++)
+    {
+        if (is_prime(i))
+        {
+            pthread_mutex_lock(&mutex);
+            primes[total] = i;
+            total++;
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+    return NULL;
+}
+
+int main(int argn, char **argv)
+{
+    int i;
+    pthread_t tids[THREADS - 1];
+    // array to save offset
+    int thread_ids[THREADS - 1];
+
+    for (i = 0; i < N; i++)
+    {
+        pflag[i] = 1;
+    }
+    for (i = 0; i < THREADS - 1; i++)
+    {
+        // Save offset
+        thread_ids[i] = i;
+        pthread_create(&tids[i], NULL, work, (void *)&thread_ids[i]);
+    }
+
+    i = THREADS - 1;
+    work((void *)&i);
+
+    // Wait for all threads to finish
+    for (i = 0; i < THREADS - 1; i++)
+    {
+        pthread_join(tids[i], NULL);
+    }
+
+    printf("Number of prime numbers between 2 and %d: %d\n",
+           N, total);
+    for (i = 0; i < total; i++)
+    {
+        printf("%d\n", primes[i]);
+    }
+
+    return 0;
+}
