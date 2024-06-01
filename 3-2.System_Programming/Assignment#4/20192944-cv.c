ddc1746241695d3582a480db3ba2a81f07598a6d
oxdjww <oxdjww@gmail.com>
2024-06-01 07:35:43 +0000

    Del mut in pflag(cv)

--- 3-2.System_Programming/Assignment#4/20192944-cv.c
+++ 3-2.System_Programming/Assignment#4/20192944-cv.c
@@ -22,13 +22,7 @@ int is_prime(int v)
             continue;
         if (v % i == 0)
         {
-            // Lock shared mem space
-            pthread_mutex_lock(&mutex);
             pflag[v] = 0;
-            // Unock shared mem space
-            pthread_mutex_unlock(&mutex);
-            // Wake other thread
-            pthread_cond_signal(&cond);
             return 0;
         }
     }
